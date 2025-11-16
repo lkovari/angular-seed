9323600a976fc432428385bf34a22c000a923360
László Kővári <laszlo.kovary@gmail.com>
2025-11-16 21:43:30 +0100

    show readme on home screen fix css bundle over 4...

--- projects/seed-app/src/app/features/home/home.component.ts
+++ projects/seed-app/src/app/features/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private http = inject(HttpClient);
+  readmeContent = signal<string>('Loading README...');
 
+  ngOnInit(): void {
+    this.http.get('/README.md', { responseType: 'text' }).subscribe({
+      next: (content) => this.readmeContent.set(content),
+      error: () => this.readmeContent.set('Failed to load README.md')
+    });
+  }
 }
